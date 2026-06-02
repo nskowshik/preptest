122065647a6d0c9ab14b20e1d0cd8bb492f297a5
Kowshik Sudarsan <aathins5@gmail.com>
2026-06-03 00:09:02 +0530

    Dashboard completed with material ui

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,24 +2,46 @@ import React, { useEffect, useState } from 'react';
 import apiClient from '../../api';
 import { Card, Table } from '../../ui';
 
+const ColumnsToPick = ['name', 'subject', 'status', 'created_at'];
 const Dashboard = () => {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
   const [tests, setTests] = useState([]);
+  const [columns, setColumns] = useState([]);
 
+
+  const formatColumns = (data) => {
+    const columns = Object.keys(data[0])
+      .filter((key) => ColumnsToPick.includes(key))
+      .map((key) => ({
+        field: key,
+        headerName: key.replace('_', ' ').toUpperCase(),
+        flex: 1,
+        ...(key === 'created_at' && {
+          type: 'dateTime',
+          valueGetter: (value) => value ? new Date(value) : null,
+          valueFormatter: (value) =>
+            value
+              ? value.toLocaleString('en-IN')
+              : '',
+        }),
+      }));
+
+    setColumns(columns);
+    setTests(data);
+  };
   const fetchAllTest = async () => {
     setLoading(true);
     try {
       const response = await apiClient.get(`/tests`);
       const data = response.data;
       if (data.status === 'success') {
-        setTests(data?.data);
+        formatColumns(data?.data);
       } else {
         setError(data.message);
         setLoading(false);
       }
     } catch (error) {
-      console.error(error);
       setError(error.message);
     } finally {
       setLoading(false);
@@ -31,40 +53,16 @@ const Dashboard = () => {
   }, []);
 
   if (loading) {
-    <div>Loading...</div>;
+    return <div>Loading...</div>;
   }
   return (
     <div className="space-y-2">
       <h1 className="text-xl font-semibold text-slate-900">Dashboard</h1>
-      <Table/>
-      <Card>
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>Song</th>
-              <th>Artist</th>
-              <th>Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-              <td>Malcolm Lockyer</td>
-              <td>1961</td>
-            </tr>
-            <tr>
-              <td>Witchy Woman</td>
-              <td>The Eagles</td>
-              <td>1972</td>
-            </tr>
-            <tr>
-              <td>Shining Star</td>
-              <td>Earth, Wind, and Fire</td>
-              <td>1975</td>
-            </tr>
-          </tbody>
-        </table>
-      </Card>
+      <Table
+        rows={tests}
+        columns={columns}
+        isLoading={loading}
+      />
     </div>
   );
 };
